a5b37e88b2306a9a3e1eae93ad4356a89aca1702
sebastienmanolios <sebastien.manolios@gmail.com>
2021-04-23 02:37:54 +0200

    login module done

--- README.md
+++ README.md
@@ -1,8 +1,42 @@
 # vuetify
 
+``` <v-spacer> </v-spacer> ```  
+is equivalent to  
+``` <div class="spacer"></div> ```  
+
+``` <v-card> ```  
+- serves as a container 
+
+``` <v-card-title> ```  
+- make the heading have proper spacing & positionning
+
+``` <v-card-text> ```  
+- serves as the body of the container
+
+``` <v-form> ```  
+
+``` <v-text-field> ```  
+form text input
+
+``` <v-card-actions> ```  
+``` <v-btn> ```  
+``` <v-spacer> ```    
+- add some inline space
+
+
+
+
+
+
+
+
+
+
+
 ## Project setup
 ```
 yarn install
+
 ```
 
 ### Compiles and hot-reloads for development
--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,33 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <!-- mx-auto : center on the page -->
+    <v-card width=400 class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">Login</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field 
+            label="Username"
+            prepend-icon="mdi-account-circle"
+            />
+          <v-text-field
+            :type="showPassword ? 'text' : 'password'"
+            label="Password"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+            />
+        </v-form>
+      </v-card-text>
+     
+      <v-card-actions>
+        <v-btn color="success">Register</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="info">Login</v-btn>
+      </v-card-actions>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    </v-card>
   </v-app>
 </template>
 
@@ -54,7 +42,7 @@ export default {
   },
 
   data: () => ({
-    //
+    showPassword: false
   }),
 };
 </script>
