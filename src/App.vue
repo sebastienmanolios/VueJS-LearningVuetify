5f034e9716ce2daa4abaf4fcd5479c88b2c539b9
sebastienmanolios <sebastien.manolios@gmail.com>
2021-04-23 16:45:24 +0200

    Add login view

--- src/App.vue
+++ src/App.vue
@@ -3,38 +3,13 @@
     <v-app-bar app color="primary" dark>
       <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn text rounded>Login</v-btn>
+      <v-btn text rounded to="/">Home</v-btn>
+      <v-btn text rounded to="/login">Login</v-btn>
+    
     </v-app-bar>
     <!-- mx-auto : center on the page -->
     <v-content>
-      <v-card width=400 class="mx-auto mt-5">
-        <v-card-title>
-          <h1 class="display-1">Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field 
-              label="Username"
-              prepend-icon="mdi-account-circle"
-              />
-            <v-text-field
-              :type="showPassword ? 'text' : 'password'"
-              label="Password"
-              prepend-icon="mdi-lock"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-              />
-          </v-form>
-        </v-card-text>
-      
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-
-      </v-card>
+      <router-view></router-view>
     </v-content>
     <v-footer
       color="primary lighten-1"
