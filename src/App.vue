b9130bdb89c1cc7e4abd715cc9ef9583173a465e
sebastienmanolios <sebastien.manolios@gmail.com>
2021-04-24 08:08:35 +0200

    Theme & Typographie

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
         :to="link.url"
         >{{ link.label }}
       </v-btn>
+      <v-btn @click="toggleTheme" text rounded>Toggle theme</v-btn>
     </v-app-bar>
     <!-- --------------------------------------------------------------------------------- -->
     <v-content>
@@ -85,5 +86,11 @@ export default {
 
     ]
   }),
+  methods : {
+    toggleTheme() {
+      this.$vuetify.theme.themes.dark.anchor = '#41B883'
+      this.$vuetify.theme.dark =! this.$vuetify.theme.dark
+    }
+  }
 };
 </script>
