6fceca35919271591aeea77a860fea1f42fd5763
sebastienmanolios <sebastien.manolios@gmail.com>
2021-04-24 07:25:46 +0200

    form validation

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,36 +1,89 @@
 <template>
   <v-container>
-    <v-raw>
+    <v-row>
       <v-col>
         <h1>Signup</h1>
-        <v-form>
-          <v-text-field label="Email" type="email"></v-text-field>
-          <v-autocomplete :items="browser" label="Which browser do you use?"></v-autocomplete>
-          <v-file-input label="Attach file profile"></v-file-input>
-          <v-text-field label="Birthday" v-model="birthday" readonly></v-text-field>
+        <v-form ref="signUpForm" v-model="formValidity">
+          <v-text-field
+            label="Email"
+            type="email"
+            v-model="email"
+            :rules="emailRules"
+            required
+          ></v-text-field>
+          <v-autocomplete
+            label="Which browser do you use?"
+            :items="browsers"
+          ></v-autocomplete>
+          <v-file-input label="Attach profile picture"></v-file-input>
+          <v-text-field
+            v-model="birthday"
+            label="Birthday"
+            readonly
+          ></v-text-field>
           <v-date-picker v-model="birthday"></v-date-picker>
-          <v-checkbox label="Agree to terms & conditions"></v-checkbox>  
-          <v-switch label="Agree to terms & conditions"></v-switch>  
-          <v-btn color="primary" type="submit">Submit</v-btn>
+          <v-checkbox
+            label="Agree to terms & conditions"
+            v-model="agreeToTerms"
+            :rules="agreeToTermsRules"
+            required
+          ></v-checkbox>
+          <v-btn
+            type="submit"
+            color="primary"
+            class="mr-4"
+            :disabled="!formValidity"
+            >Submit</v-btn
+          >
+          <v-btn color="success" class="mr-4" @click="validateForm"
+            >Validate Form</v-btn
+          >
+          <v-btn color="warning" class="mr-4" @click="resetValidation"
+            >Reset Validation</v-btn
+          >
+          <v-btn color="error" @click="resetForm">Reset</v-btn>
         </v-form>
       </v-col>
-    </v-raw>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      birthday: '',
-     browser: [
-       'Chrome',
-       'Firefox',
-       'Safari',
-       'Edge',
-       'Brave'
-     ]
+  data: () => ({
+    agreeToTerms: false,
+    agreeToTermsRules: [
+      value =>
+        !!value ||
+        'You must agree to the terms and conditions to sign up for an account.'
+    ],
+    birthday: '',
+    browsers: ['Chrome', 'Firefox', 'Safari', 'Edge', 'Brave'],
+    email: '',
+    emailRules: [
+      value => !!value || 'Email is required.',
+      value => value.indexOf('@') !== 0 || 'Email should have a username.',
+      value => value.includes('@') || 'Email should include an @ symbol.',
+      value =>
+        value.indexOf('.') - value.indexOf('@') > 1 ||
+        'Email should contain a valid domain.',
+      value => value.includes('.') || 'Email should include a period symbol.',
+      value =>
+        value.indexOf('.') <= value.length - 3 ||
+        'Email should contain a valid domain extension.'
+    ],
+    formValidity: false
+  }),
+  methods: {
+    resetForm() {
+      this.$refs.signUpForm.reset()
+    },
+    resetValidation() {
+      this.$refs.signUpForm.resetValidation()
+    },
+    validateForm() {
+      this.$refs.signUpForm.validate()
     }
-  },
+  }
 }
 </script>
